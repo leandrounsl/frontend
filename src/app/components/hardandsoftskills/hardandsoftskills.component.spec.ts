a3c501c5288254393187369a81dcb0d2cc69951a
Iviss2580 <iviss036@hotmail.com>
2022-07-14 11:07:57 -0300

    nuevo repositorio con todo el front end hasta 13dejulio

---
+++ src/app/components/hardandsoftskills/hardandsoftskills.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HardandsoftskillsComponent } from './hardandsoftskills.component';
+
+describe('HardandsoftskillsComponent', () => {
+  let component: HardandsoftskillsComponent;
+  let fixture: ComponentFixture<HardandsoftskillsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HardandsoftskillsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HardandsoftskillsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
