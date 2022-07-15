b1734da1ab97e93c1ef4407c9fe379b6f698c850
Iviss2580 <iviss036@hotmail.com>
2022-07-15 11:36:48 -0300

    login y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HardandsoftskillsComponent } from './components/hardandsoftskills/harda
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule} from '@angular/common/http';
     EducacionComponent,
     HardandsoftskillsComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
